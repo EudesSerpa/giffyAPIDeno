8a74cd9451ed6698f80a8d96f17ceb04a1ae6b23
Eudes Serpa <serpagame@gmail.com>
2022-09-11 13:27:27 -0500

    Test access to the env variables with Deno

--- database/connection.ts
+++ database/connection.ts
@@ -1,16 +1,27 @@
-import { MongoClient, config } from "../deps.ts";
+import { MongoClient } from "../deps.ts";
+// import { MongoClient, config } from "../deps.ts";
 
-const { CLUSTER_URL, DATABASE, USERNAME_DB, PASSWORD_DB } = config();
+// const { CLUSTER_URL, DATABASE, USERNAME_DB, PASSWORD_DB } = config();
 
 const client = new MongoClient();
 
 try {
+  // Environment Variablesin Deno Deploy
   await client.connect(
-    `mongodb+srv://${USERNAME_DB}:${PASSWORD_DB}@${CLUSTER_URL}/${DATABASE}?authMechanism=SCRAM-SHA-1`
+    `mongodb+srv://${Deno.env.get("USERNAME_DB")}:${Deno.env.get(
+      "PASSWORD_DB"
+    )}@${Deno.env.get("CLUSTER_URL")}/${Deno.env.get(
+      "DATABASE"
+    )}?authMechanism=SCRAM-SHA-1`
   );
+
+  // Connection with Environment Variables with dotEnv library (it works in local)
+  // await client.connect(
+  //   `mongodb+srv://${USERNAME_DB}:${PASSWORD_DB}@${CLUSTER_URL}/${DATABASE}?authMechanism=SCRAM-SHA-1`
+  // );
 } catch (err) {
   console.error("Error connecting to MongoDB", err);
-  throw err;
+  throw err; // To chaning the error
 }
 
-export const db = client.database(DATABASE);
+export const db = client.database(Deno.env.get("DATABASE"));
